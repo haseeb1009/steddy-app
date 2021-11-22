3e953a12263b1f6045e294f4444ace0623d4520b
Raffi Chilingaryan <raffi.chilingaryan@gmail.com>
2021-11-22 13:37:18 -0600

    misc updates
    lookin better

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 // import qrCode from "../qr-code.svg";
-import qrCode from "../qr-code-2.svg";
+
 // import signal from "../signal.svg";
 import signal from "../signal-2.svg";
 import plan1 from "../plan1.svg";
@@ -13,6 +13,9 @@ import img4 from "../img4.svg";
 import colorful from "../colorful.jpg";
 import appStore from "../app-store.svg";
 import playStore from "../play-store.svg";
+
+// QR CODE
+import qrCode from "../qr-code-4.svg";
 
 import Footer from "../components/Footer/Footer";
 import "./Main.scss";
@@ -28,14 +31,7 @@ const Main = () => {
 
   return (
     <div className="app">
-      <div className = "shell" 
-        //   style = {{
-        //     backgroundImage: 'url("../colorful.jpg")',
-        //     width: '100%', height: '100%',
-        //     backgroundRepeat: 'no-repeat',
-        //     backgroundAttachment: 'fixed'
-        // }}
-        >
+      <div className = "shell" >
       <div className="section1" >
         <p>
           It’s time to go <br /> <strong className="title"> steddy </strong> with your <br /> exercise routine.
@@ -50,12 +46,28 @@ const Main = () => {
 
       <div className="section2" >
         <img src={signal} alt="Signal" />
-        <p> Steddy is the simplest way to build a weekly exercise plan.
+        <p> 
+        Steddy is the 
+        {' '}
+        <span style = {{ 
+          fontSize: 'calc(11px + 1.2vw)', 
+          backgroundColor: '#e8d4f9', 
+          color: '#4E0591', 
+          // backgroundColor: '#E3FDF0', 
+          // color: '#103B26', 
+          padding: '2px 10px', 
+          borderRadius: '5px', 
+          fontWeight: '600',
+          borderWidth: '3px',
+          borderColor: '#103B26', 
+          }}>
+          free and simple
+        </span> 
+        {' '}
+        way to build a weekly exercise plan.
           <br />
-            <span style = {{ color: 'black', textDecoration: 'underline', fontWeight: 700 }}>
-            Scan the QR code 
-            </span>
-          {' '}to see how far a little commitment can take you.
+            <span style = {{ fontSize: 'calc(14px + 1.5vw)', marginRight: '6px'}}>📷  </span>
+            Scan the QR code to see how far a little commitment can take you.
         </p>
       </div>
 
@@ -67,18 +79,22 @@ const Main = () => {
             <img className="icon" src={img1} alt="Hammer" /> 
             <h2 className="heading">Plan Flexibly</h2>
             <p className="description">
-              <span style = {{ color: 'black', backgroundColor: '#e8d4f9', fontWeight: 700 }}>Plan any type of workout</span>{' '}
+              <span style = {{ color: 'black', fontWeight: 700 }}>Plan any type of workout</span>{' '}
               be it yoga class, leg day, or your company softball game.</p>
             <p className="mobile-description">
-              <span style = {{ color: 'black', backgroundColor: '#e8d4f9', fontWeight: 700 }}>Plan any type of workout</span>{' '}
+              <span style = {{ color: 'black', fontWeight: 700 }}>Plan any type of workout</span>{' '}
               be it yoga class, leg day, or your company softball game.</p>
           </div>
           <div className="details-container reverse">
             {/* MAGNIFYING GLASS */}
             <img className="icon" src={img2} alt="Magnifier" />
             <h2 className="heading">Find Routines</h2>
-            <p className="description">Not sure where to start? Explore our presets to find plans based on the results you're looking for.</p>
-            <p className="mobile-description">Not sure where to start? Explore our presets to find plans based on the results you're looking for.</p>
+            <p className="description">Not sure where to start? 
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>Explore our presets to find plans</span>{' '}
+            based on the results you're looking for.</p>
+            <p className="mobile-description">Not sure where to start? 
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>Explore our presets to find plans</span>{' '}
+            based on the results you're looking for.</p>
           </div>
         </div>
         <img className="section3-image" src={plan1} alt="SCREENSHOT" />
@@ -88,18 +104,28 @@ const Main = () => {
         <img className="section4-image" src={plan2} alt="SCREENSHOT" />
         <div>
           <div className="details-container">
-            <h2 className="heading">Kick Back</h2>
-            <p className="description">Stay intentional about your exercise and your sanity by scheduling unplanned time :D</p>
-            <p className="mobile-description">Stay intentional about your exercise and your sanity by scheduling unplanned time :D</p>
-            {/* PALM TREE */}
-            <img src={img3} alt="palmTree" className="icon" />
-          </div>
-          <div className="details-container reverse">
-            <h2 className="heading">Track Progress</h2>
-            <p className="description">We track your streaks, follow-through, and more so you know exactly how you're doing.</p>
-            <p className="mobile-description">We track your streaks, follow-through, and more so you know exactly how you're doing.</p>
+          <h2 className="heading">Track Progress</h2>
+            <p className="description">
+            We track your
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>streaks, follow-through, and more</span>{' '}
+            so you know exactly how you're doing.</p>
+            <p className="mobile-description">
+            We track your
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>streaks, follow-through, and more</span>{' '}
+            so you know exactly how you're doing.</p>
             {/* DATA */}
             <img src={img4} alt="data" className="icon" />
+          </div>
+          <div className="details-container reverse">
+          <h2 className="heading">Kick Back</h2>
+            <p className="description">
+            Stay intentional about your exercise and your sanity 
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>by scheduling unplanned time :)</span></p>
+            <p className="mobile-description">
+            Stay intentional about your exercise and your sanity 
+            {' '}<span style = {{ color: 'black', fontWeight: 700 }}>by scheduling unplanned time :)</span></p>
+            {/* PALM TREE */}
+            <img src={img3} alt="palmTree" className="icon" />
           </div>
         </div>
       </div>
